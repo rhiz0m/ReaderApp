ebc67376d5acfbf8a5c7082ea7c09630635ebb70
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-06-22 18:52:20 +0200

    First commit. Reader App

---
+++ ReaderApp/ReaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderAppApp.swift
+//  ReaderApp
+//
+//  Created by Andreas Antonsson on 2024-06-22.
+//
+
+import SwiftUI
+
+@main
+struct ReaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
