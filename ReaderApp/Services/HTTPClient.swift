4a83bcdfa4a16a95e6fe3cd3b833bed77e16472c
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-07-24 18:30:25 +0200

    Feat - Network layer. Start working on  HTTPClient

---
+++ ReaderApp/Services/HTTPClient.swift
@@ -0,0 +1,20 @@
+//
+//  HTTPClient.swift
+//  ReaderApp
+//
+//  Created by Andreas Antonsson on 2024-07-24.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case badUrl
+    case noData
+    case decodingError
+}
+
+struct HTTPClient {
+    func getTextsBy() {
+        
+    }
+}
